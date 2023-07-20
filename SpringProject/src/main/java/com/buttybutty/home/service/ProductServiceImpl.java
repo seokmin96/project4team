6db64edb5640fd795f6054194550711a0cb102b7
minjee <minjee3333@naver.com>
2023-07-20 15:02:41 +0900

    ;;

--- SpringProject/src/main/java/com/buttybutty/home/service/ProductServiceImpl.java
+++ SpringProject/src/main/java/com/buttybutty/home/service/ProductServiceImpl.java
@@ -3,12 +3,14 @@ package com.buttybutty.home.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.buttybutty.home.dao.ProductDAO;
 import com.buttybutty.home.dto.BoardDTO;
 import com.buttybutty.home.dto.PagingDTO;
 import com.buttybutty.home.dto.ProductDTO;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 	@Autowired
 	ProductDAO dao;
